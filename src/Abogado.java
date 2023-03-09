6a35f2003dbd8add71c011a2070b37d12a700cca
Jesús Barrajón <JesusFernando.Barrajon@alu.uclm.es>
2023-03-09 11:10:48 +0100

    Creación de clases EMpleado y Abogado

---
+++ src/Abogado.java
@@ -0,0 +1,16 @@
+public class Abogado extends Empleado{
+
+    @Override
+    public int getVacaciones (){
+        return super.getVacaciones() / 2;
+    }
+
+    @Override
+    public double getSalario (){
+        return super.getSalario() + 1000;
+    }
+
+
+
+
+}
